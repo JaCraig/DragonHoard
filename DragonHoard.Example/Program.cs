a6e8250dcd54ee3af045f5cf6c37e68abc9ad6b8
JaCraig <craig.jamesa@gmail.com>
2023-07-11 11:17:00 -0400

    fix: Setting up automation and updating packages

---
+++ DragonHoard.Example/Program.cs
@@ -0,0 +1,54 @@
+﻿using DragonHoard.Core;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DragonHoard.Example
+{
+    /// <summary>
+    /// Example class.
+    /// </summary>
+    internal class ExampleClass
+    {
+        /// <summary>
+        /// Gets or sets a.
+        /// </summary>
+        /// <value>
+        /// a.
+        /// </value>
+        public int A { get; set; }
+    }
+
+    /// <summary>
+    /// Example program.
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        private static void Main(string[] args)
+        {
+            // Create our service provider.
+            var ServiceProvider = new ServiceCollection().AddCanisterModules()?.BuildServiceProvider();
+
+            // Get our cache provider.
+            var CacheProvider = ServiceProvider?.GetService<Cache>();
+
+            if (CacheProvider is null)
+                return;
+
+            // Get our cache. This will create a new cache if one doesn't exist with the name "Default".
+            var Cache = CacheProvider.GetOrAddCache();
+
+            if (Cache is null)
+                return;
+
+            // Add our data to the cache.
+            Cache.Set("Test", new ExampleClass { A = 12 });
+
+            // Check if our data is in the cache and retrieve it.
+            Console.WriteLine("Data found: {0}", Cache.TryGetValue("Test", out ExampleClass value));
+            Console.WriteLine("Our stored data: {0}", value.A);
+        }
+    }
+}
---
+++ docfx_project/articles/intro.md
@@ -0,0 +1,9 @@
+# Code
+[!code-csharp[](../../DragonHoard.Example/Program.cs)]
+
+# Output
+
+```
+Data found: True
+Our stored data: 12
+```
